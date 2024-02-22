26a770cae73de5f3082c0e759f1cb88997afc52a
Татьяна Прудьева <AlexRyzh@macbook-pro--asd.local>
2024-02-22 15:35:10 +0300

    Initial Commit

---
+++ TestGitHab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGitHab
+//
+//  Created by Alexander on 22.02.2024.
+//  Copyright © 2024 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
